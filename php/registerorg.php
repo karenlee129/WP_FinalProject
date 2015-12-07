f6f3bee53072bb510411c18b8281540e700cbcae
Karen Lee <karenlee129@utexas.edu>
2015-12-06 19:11:15 -0600

    registerorg.php

--- php/registerorg.php
+++ php/registerorg.php
@@ -1,138 +1,63 @@
 <?php
-$group = $_POST["orgname"];
-$des = $_POST["Description"];
 
+#connect to mysql#############################################################
+//read password stored not on the web server
+$file = fopen("./sqlinfo.txt", "r");
+$line = trim(fgets($file));
+fclose($file);
+
+// Connect to the MySQL database
 $host = "fall-2015.cs.utexas.edu";
-$user = "ronly";
-$pwd = "WKaCjv92wl";
-$dbs = "cs329e_ronly";
+$user = "karen129";
+$pwd = "$line";
+$dbs = "cs329e_karen129";
 $port = "3306";
 
 $connect = mysqli_connect ($host, $user, $pwd, $dbs, $port);
-$table = "registration";
 
-$sql = "INSERT INTO $table VALUES (\"$group\", \"$des\")";
+if (empty($connect))
+{
+  die("mysqli_connect failed: " . mysqli_connect_error());
+}
+ 
+############################################################################
+if (isset($_POST["registerorg"])){
+	unset($_POST["registerorg"]);
+	$name = $_POST["name"];
+	$description = $_POST["description"];
 
-if(mysqli_query ($connect, $sql)){
-    registerorg();
-    }
-
-function registerorg(){
-    print <<<HP_Registration
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <title>Scrolling Nav - Start Bootstrap Template</title>
-
-        <!-- Bootstrap Core CSS -->
-        <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="../css/scrolling-nav.css" rel="stylesheet">
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-
-    </head>
-
-    <!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
-
-    <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container">
-                <div class="navbar-header page-scroll">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand page-scroll" href="../index.html">Gather</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav">
-                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                        <li class="hidden">
-                            <a class="page-scroll" href="#page-top"></a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#about">About</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#services">Services</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a class="page-scroll navbar-right" href="#login">
-                                <form method = "POST" action = "./php/login.php">
-                                    <table border = "0">
-                                        <tr>
-                                            <td> Username : </td>
-                                            <td> <input type = "text" name = "user" id = "r_user" size = "20"/> </td>
-                                            <td> Password : </td>
-                                            <td> <input type = "password" name = "pass" size = "20"/> </td>
-                                            <td> <input type = "submit" value = "Login" /> </td>
-                                        </tr>
-                                    </table>
-                                </form>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-            <!-- /.container -->
-        </nav>
-
-        <!-- Intro Section -->
-        <section id="intro" class="intro-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1>Thank you for you feedback!!</h1>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <!-- jQuery -->
-        <script src="../js/jquery.js"></script>
-
-        <!-- Bootstrap Core JavaScript -->
-        <script src="../js/bootstrap.min.js"></script>
-
-        <!-- Scrolling Nav JavaScript -->
-        <script src="../js/jquery.easing.min.js"></script>
-        <script src="../js/scrolling-nav.js"></script>
-
-    </body>
-
-    </html>
-HP_Registration;
-
-
+	if(strlen($name) !==0 and strlen($description) !==0){
+		$stmt = mysqli_prepare($connect, "insert into Organizations values (?, ?)");
+		mysqli_stmt_bind_param($stmt, 'ss', $name, $description);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_close($stmt);
+	}
+} else {
+	registerorg();
 }
 
+function RegisterOrg(){
+	$script = $_SERVER['PHP_SELF'];
+
+	print<<<Register
+	<form action = "$script" method = "post">
+	<table border = "0">
+	  <tr>
+	  <td> Organization Name </td>
+	  <td> <input type = "text" name = "name" /> </td>
+	  </tr>
+	  <tr>
+	  <td> Organization Description </td>
+	  <td> <input type = "textarea" name = "description" /> </td>
+	  </tr>
+	  <tr>
+	  <td> <input type = "submit" name = "RegisterOrg" value = "Register" /> </td>
+	  </tr>
+	</table>
+	</form>
+Register;
+  
+}
 
 
 ?>
